0ea00f9afeb20b8085e5b6d9e64eb3c3e43cadbd
LasymAndIris <46901347+LasymAndIris@users.noreply.github.com>
2020-05-14 19:40:21 +0200

    Testing things with JavaScript

---
+++ player.js
@@ -0,0 +1,17 @@
+function getText(url){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+        }
+    }
+}
+
+var text = getText('file:///D:/Lasym/Programmation/GitHub/Website/lasymandiris.github.io/test.json');
+console.log(text);
